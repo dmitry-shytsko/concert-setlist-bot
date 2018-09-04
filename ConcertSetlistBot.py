1d9642aead9afb1cae1566894238bcd635f38219
Dmitry Shytsko <dmitry.shytsko@gmail.com>
2018-09-04 23:52:43 +0300

    Added support for /setlist command - will be useful in group chats

--- ConcertSetlistBot.py
+++ ConcertSetlistBot.py
@@ -29,29 +29,53 @@ def whatsNewCommandHandler(bot, update):
     whatsNewFile.close()
 
 
+def setlistCommandHandler(bot, update):
+    entities = update.message.parse_entities()
+
+    if len(entities) == 1:
+        entityList = list(entities.keys())
+        entity = entityList[0]
+
+        if entity.type == "bot_command":
+            setlistRequest = update.message.text[entity.offset+entity.length : len(update.message.text)].strip()
+
+            if len(setlistRequest) > 0:
+                processSetlistRequest(setlistRequest, bot, update)
+            else:
+                update.message.reply_text("/setlist command has to be followed by the artist name")
+
+            return 
+
+    update.message.reply_text("None Found")
+
+
 def artistNameHandler(bot, update):
     if update.message.text is not None and len(update.message.text) > 0:
-        setlistParams = Setlist.SetlistParams(update.message.text)
-        artistSetlists = Setlist.Setlists(setlistParams.artist, config)
-
-        if update.message.from_user is not None:
-            logger.info('Setlist for "%s" requested by "%s"', setlistParams.artist, update.message.from_user)
-        else:
-            logger.info('Setlist for "%s" requested by unknown user', setlistParams.artist)
-
-        if artistSetlists.code == 0:
-            setlistsToDisplay = artistSetlists.getMostRecentSetlists(setlistParams.count)
-
-            if len(setlistsToDisplay) > 0:
-                setlistsToDisplayIterator = iter(setlistsToDisplay)
-                for setlistToDisplay in setlistsToDisplayIterator:
-                    bot.send_message(update.message.chat_id, setlistToDisplay.setlistAsHtml(), "HTML")
-            else:
-                update.message.reply_text("None Found")
-        else:
-            update.message.reply_text(artistSetlists.status)
+        processSetlistRequest(update.message.text.strip(), bot, update)
     else:
         update.message.reply_text("Nothing to look for")
+
+
+def processSetlistRequest(request, bot, update):
+    setlistParams = Setlist.SetlistParams(request)
+    artistSetlists = Setlist.Setlists(setlistParams.artist, config)
+
+    if update.message.from_user is not None:
+        logger.info('Setlist for "%s" requested by "%s"', setlistParams.artist, update.message.from_user)
+    else:
+        logger.info('Setlist for "%s" requested by unknown user', setlistParams.artist)
+
+    if artistSetlists.code == 0:
+        setlistsToDisplay = artistSetlists.getMostRecentSetlists(setlistParams.count)
+
+        if len(setlistsToDisplay) > 0:
+            setlistsToDisplayIterator = iter(setlistsToDisplay)
+            for setlistToDisplay in setlistsToDisplayIterator:
+                bot.send_message(update.message.chat_id, setlistToDisplay.setlistAsHtml(), "HTML")
+        else:
+            update.message.reply_text("None Found")
+    else:
+        update.message.reply_text(artistSetlists.status)
 
 
 # noinspection PyUnusedLocal
@@ -71,6 +95,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler("start", startCommandHandler))
     updater.dispatcher.add_handler(CommandHandler("help", helpCommandHandler))
     updater.dispatcher.add_handler(CommandHandler("whatsnew", whatsNewCommandHandler))
+    updater.dispatcher.add_handler(CommandHandler("setlist", setlistCommandHandler))
     updater.dispatcher.add_handler(MessageHandler(Filters.text, artistNameHandler))
     updater.dispatcher.add_error_handler(errorHandler)
 
